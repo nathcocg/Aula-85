e2f3f6a7a61405f410f05a6827d00c6c88e4e92c
Nathália de Carvalho Oliveira Cunha Guimarães <96549422+nathcocg@users.noreply.github.com>
2023-02-22 18:26:36 -0300

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import StoryCard from "./StoryCard";
+
+import * as Font from "expo-font";
+import { FlatList } from "react-native-gesture-handler";
+
+import * as SplashScreen from 'expo-splash-screen';
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+let stories = require("./temp_stories.json");
+
+export default class Feed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  renderItem = ({ item: story }) => {
+    return <StoryCard story={story} navigation={this.props.navigation} />;
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
+  render() {
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync();
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>App Narração de Histórias</Text>
+            </View>
+          </View>
+          <View style={styles.cardContainer}>
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={stories}
+              renderItem={this.renderItem}
+            />
+          </View>
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(22),
+    fontFamily: "Bubblegum-Sans"
+  },
+  cardContainer: {
+    flex: 0.85
+  }
+});
